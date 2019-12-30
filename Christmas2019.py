78499e5d8965bc4f289ff836233aeece1f920599
Hugh <det.nosnibor00@gmail.com>
2019-12-29 21:49:03 -0600

    Create Christmas2019.py
    Added the code from AWS lambda for the application I ran during Christmas 2019 for our family scavenger hunt.

---
+++ Christmas2019.py
@@ -0,0 +1,254 @@
+"""
+This is a Python template for Alexa to get you building skills (conversations) quickly.
+"""
+
+from __future__ import print_function
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+def get_tj_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "TJ"
+    speech_output = "Hello TJ, your first clue is located in the top left drawer of the entertainment center"
+    reprompt_text = "again TJ's first clue is in the top left drawer of the entertainment center"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_daniel_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "Daniel"
+    speech_output = "Hello Daniel, your first clue is located in the monopoly board game box"
+    reprompt_text = "again Daniel's first clue is in the monopoly board game box"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_bless_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "Bless"
+    speech_output = "Hello Bless, your first clue is located on top of the refridgerator"
+    reprompt_text = "again Bless' first clue is on top of the refridgerator"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_gie_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "Gie"
+    speech_output = "Hello Rock rock, your first clue is located under the living room carpet"
+    reprompt_text = "again Gie's first clue is under the living room carpet"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_tj_pass_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "tj_pass"
+    speech_output = "Great job finding your first clue TJ!  Your second clue is under the ironing board in your parent's bedroom"
+    reprompt_text = "again TJ's first clue is under the ironing board in your parent's bedroom"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_daniel_pass_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "daniel_pass"
+    speech_output = "Excellent work finding your first clue Daniel! Your second clue is located in TJ's bay blade bin in his room"
+    reprompt_text = "again Daniel's second clue is inside TJ's bay blade bin in his room"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_bless_pass_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "bless_pass"
+    speech_output = "Stellar performance Bless in finding your first clue! Your second clue is located on the shoe rack in TJ's blue shoe"
+    reprompt_text = "again TJ's second clue is on the shoe rack in TJ's blue shoe"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_gie_pass_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "gie_pass"
+    speech_output = "rock rock, I am impressed with your searching abilities! Your second clue is located under your bed"
+    reprompt_text = "again Geraldine's second clue is under her bed"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def try_again_response():
+    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "try_again"
+    speech_output = "I did not understand.  Try again"
+    reprompt_text = "I'm still having trouble.  Please speak slower"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Merry Christmas Robinson family. I am going to help you find your first clue.  Say something like this is Daniel where is my first clue"
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "I don't know if you heard me, Merry Christmas Robinson family!"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts.
+        One possible use of this function is to initialize specific 
+        variables from a previous state stored in an external database
+    """
+    # Add additional code here as needed
+    pass
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+    # Dispatch to your skill's launch message
+    return get_welcome_response()
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "TJ":
+        return get_tj_response()
+    elif intent_name == "Daniel":
+        return get_daniel_response()
+    elif intent_name == "Bless":
+        return get_bless_response()
+    elif intent_name == "Gie":
+        return get_gie_response()
+    elif intent_name == "tj_pass":
+        return get_tj_pass_response()
+    elif intent_name == "daniel_pass":
+        return get_daniel_pass_response()
+    elif intent_name == "bless_pass":
+        return get_bless_pass_response()
+    elif intent_name == "gie_pass":
+        return get_gie_pass_response()    
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        #raise ValueError("Invalid intent")
+        return try_again_response()
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("Incoming request...")
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
